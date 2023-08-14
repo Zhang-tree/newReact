9837c43c6d8a82ea09700c0d0a7b3130adff1da0
Zhang-tree <1071066290@qq.com>
2023-08-14 12:11:44 +0800

    恢复初始

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          张云海Learn React测试3
+          Learn React
         </a>
       </header>
     </div>
